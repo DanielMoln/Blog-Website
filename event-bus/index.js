8dd200391c33da5ef2722e97893b5e3af74a411c
Daniel Molnar <molnard.work@gmail.com>
2023-11-04 11:53:09 +0100

    A mini microservice app

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,14 +7,23 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) =>{
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event).catch((e) => console.log(e));
-    axios.post('http://localhost:4001/events', event).catch((e) => console.log(e));
-    axios.post('http://localhost:4002/events', event).catch((e) => console.log(e));
+    events.push(event);
+
+    axios.post('http://localhost:4000/events', event).catch((e) => console.log(e)); // post service
+    axios.post('http://localhost:4001/events', event).catch((e) => console.log(e)); // comments service
+    axios.post('http://localhost:4002/events', event).catch((e) => console.log(e)); // query service
+    axios.post('http://localhost:4003/events', event).catch((e) => console.log(e)); // moderation service
 
     res.send({ status: 'Ok' })
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 })
 
 app.listen(4005, () => {
